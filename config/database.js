1f8da08d77b5fdbbee3fc225c24875cb948ce137
Tristan Bridge <tbridge360@gmail.com>
2023-04-05 17:16:27 +0200

    new content types on strapi

--- config/database.js
+++ config/database.js
@@ -5,8 +5,12 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
+        // host: env('DATABASE_HOST', '127.0.0.1'),
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
+        /* database: env('DATABASE_NAME', 'pup'),
+        username: env('DATABASE_USERNAME', 'pup'),
+        password: env('DATABASE_PASSWORD'), */
         database: env('DATABASE_NAME', 'demodb'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'admin'),
